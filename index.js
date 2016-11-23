0731660eb07dbfcc3ee29aaa43de6c7046c89a7a
Jesse Rosenberger <github@jro.cc>
2016-11-23 13:19:41 +0200

    Hashing.

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
+var fs = require("fs");
 var request = require('request');
-request.debug = true;
-require('request-debug')(request);
+// request.debug = true;
+// require('request-debug')(request);
+
+var crypto = require('crypto');
+
+var hash = {};
 
 var somePackage = "https://d3fm2vapipm3k9.cloudfront.net/builds/EXSxwGqYjjJKh3WMJ/1477416387312/oW5pojeAuq/fastclick-1.0.13-os+web.browser+web.cordova.tgz";
+
+function initHash(test) {
+  return hash[test] || (hash[test] = crypto.createHash('md5'));
+}
+
+function processError(test, error) {
+  console.error("ERROR", error);
+}
+
+function processResponse(test, response) {
+  delete response.body;
+  console.log(" %s Response callback", test);
+}
+
+initHash("A");
+request(somePackage)
+  .on('error', error => processError("A", error))
+  .on('data', function(data) {
+    hash.A.update(data);
+  })
+  .on('response', response => processResponse("A", response))
+  .pipe(fs.createWriteStream('testA.tgz'))
+  .on('finish', () => {
+    console.log("HashA is", hash.A.digest("hex"))
+  })
+;
+
+initHash("B");
 request(somePackage, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else if (!error && response.statusCode == 200) {
-    console.log("OK: %d bytes", body.length);
-  }
-});
+    if (error) {
+      processError("B", error);
+    } else {
+      processResponse("B", response);
+      hash.B.update(body);
+      console.log("HashB is", hash.B.digest("hex"))
+    }
+  }).pipe(fs.createWriteStream("testB.tgz"));
