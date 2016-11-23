846e5c89b2db5a9ce0853e1a96456c8f0621b0ed
Jesse Rosenberger <github@jro.cc>
2016-11-23 13:19:41 +0200

    Try to prove that keepAlive is to blame.
    The problem also doesn't exist on Node 0.10 (first shows in 0.12, which changed something about the way `Keep-Alive` worked.
    
    If I create my own `require('https').Agent` with `keepAlive: true`, then the problem goes away. If I instead set `keepAlive: false` on that agent - it fails.  If I don't pass my own agent (and let it default to whatever it does) it fails.
    
    If I change the Network implementation of my VirtualBox to "Bridged Adapter" instead of NAT, the problem goes away.
    
    This finally proved to me that it was a networking issue.  Interestingly, setting `keepAlive: true` fixes it, but I'm not sure what the implications of doing that are.  This leads me to this bug:
    
    https://www.virtualbox.org/ticket/14049
    
    Joy? Maybe. I guess don't use "NAT" networking in VirtualBox.

--- index.js
+++ index.js
@@ -1,12 +1,76 @@
+var fs = require("fs");
 var request = require('request');
-request.debug = true;
-require('request-debug')(request);
+// request.debug = true;
+// require('request-debug')(request);
+
+var crypto = require('crypto');
+
+var hash = {};
 
 var somePackage = "https://d3fm2vapipm3k9.cloudfront.net/builds/EXSxwGqYjjJKh3WMJ/1477416387312/oW5pojeAuq/fastclick-1.0.13-os+web.browser+web.cordova.tgz";
-request(somePackage, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else if (!error && response.statusCode == 200) {
-    console.log("OK: %d bytes", body.length);
-  }
+
+function initHash(test) {
+  return hash[test] || (hash[test] = crypto.createHash('md5'));
+}
+
+function processError(test, error) {
+  console.error("ERROR", error);
+}
+
+function processResponse(test, response) {
+  delete response.body;
+  console.log(" %s Response callback", test);
+}
+
+function hashUpdate(test, data) {
+  return initHash(test).update(data);
+}
+
+function finish(test) {
+  console.log("Hash%s is", test, hash[test].digest("hex"));
+}
+
+var http = require('http');
+var https = require('https');
+
+var agentHttp = new http.Agent({ keepAlive: true });
+var agentHttps = new https.Agent({ keepAlive: true });
+
+var requestOptions = {
+  url: somePackage,
+  // agent: agentHttps,
+  encoding: null,
+}
+
+function doTest(test, f) {
+  var outputFile = "test" + test + ".tgz";
+  f(test, outputFile);
+}
+
+doTest("A", function (test, outputFile) {
+  initHash(test);
+  request(requestOptions)
+    .on('error', function (error) { processError(test, error); })
+    .on('data', function(data) { hashUpdate(test, data); })
+    .on('response', function (response) { processResponse(test, response); })
+    .pipe(fs.createWriteStream(outputFile))
+    .on('finish', function () { finish(test); })
+  ;
+});
+
+doTest("B", function (test, outputFile) {
+  initHash(test);
+  request(
+    requestOptions,
+    function (error, response, body) {
+      if (error) {
+        processError(test, error);
+      } else {
+        processResponse(test, response);
+        fs.writeFile(outputFile, body);
+        hashUpdate(test, body);
+        finish(test);
+      }
+    }
+  );
 });
